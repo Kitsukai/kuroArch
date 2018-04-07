44b9be9b425d1d1ac01b1da86234f79f1c03c14a
unknown <kitsukai@protonmail.com>
2018-04-06 21:57:33 -0500

    Some tweaks on code

--- shiroChroot.sh
+++ shiroChroot.sh
@@ -13,6 +13,8 @@ function setKbEs() { echo "KEYMAP=es" > /etc/vconsole.conf ;}
 function getVim() { pacman --noconfirm --needed -S vim ;}
 function getZen() { pacman --noconfirm --needed -S linux-zen linux-zen-headers ;}
 function zenOnly() { pacman -Rscn linux ;}
+function askForOnlyZen() { dialog --title "kuroArch Tweaks" --yesno "Do you want remove the generic linux kernel?"  8 60 && zenOnly ;}
+function zenSystem() { getZen && askForOnlyZen ;}
 #install and enable networkmanager
 pacman --noconfirm --needed -S networkmanager
 systemctl enable NetworkManager
@@ -21,7 +23,7 @@ systemctl start NetworkManager
 #ask for little tweaks
 dialog --title "kuroArch Tweaks" --yesno "Do you want set keybord layout to es?"  8 60 && setKbEs
 dialog --title "kuroArch Tweaks" --yesno "Do you want install vim?"  8 60 && getVim
-dialog --title "kuroArch Tweaks" --yesno "Do you want install the linux-zen kernel?"  8 60 && getZen
-dialog --title "kuroArch Tweaks" --yesno "Do you want remove the generic linux kernel?"  8 60 && zenOnly
+dialog --title "kuroArch Tweaks" --yesno "Do you want install the linux-zen kernel?"  8 60 && zenSystem
+
 
 pacman --noconfirm --needed -S grub efibootmgr && grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB && grub-mkconfig -o /boot/grub/grub.cfg
