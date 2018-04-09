869ae563cf9787ae44e14d6e52a6207a92ba61af
unknown <kitsukai@protonmail.com>
2018-04-09 00:47:00 -0500

    Aded time zone select
    also aded format select for root and home partitions, network manager
    installation is now other tweak

--- kuroArch.sh
+++ kuroArch.sh
@@ -8,6 +8,11 @@ pacman -S --noconfirm dialog || (echo "Error at script start: Are you sure you'r
 dialog --defaultno --title "Welcome to kuroArch" --yesno "This is an Arch install script that is very rough around the edges.\n\nOnly run this script if you're a big-brane.\n\nThis script is for me, but you can use it and modify it as you want.\n\nt. Kai"  15 60 || exit
 #save the hostname
 dialog --no-cancel --inputbox "Enter a name for your computer.(hostname, not user name)" 10 60 2> comp
+#select time zone
+dialog --defaultno --title "Time Zone select" --yesno "Do you want use the default time zone(America/New_York)?.\n\nPress no for select your own time zone"  10 60 && echo "America/New_York" > tz.tmp || tzselect > tz.tmp
+#select file system for root and home
+dialog --no-cancel --menu "Select the format of root partition:" 12 30 3 2 ext2 3 ext3 4 ext4 2>tempFRoot
+dialog --no-cancel --menu "Select the format of home partition:" 12 30 3 2 ext2 3 ext3 4 ext4 2>tempFHome
 #enable ntp
 timedatectl set-ntp true
 #define some functions
@@ -15,10 +20,13 @@ function finalreboot() { umount -R /mnt && swapoff /dev/sda2 && reboot ;}
 #formatting and mounting partitions and create needed directories
 #formatting boot as fat32 mounted at /mnt/boot
 #formatting swap as swap and enable swap
-#formattingroot as ext4 mounted at /mnt
+#formatting root as ext4 mounted at /mnt
 #formatting home as ext4 mounted at /mnt/home
-mkfs.ext4 /dev/sda4
-mkfs.ext4 /dev/sda3
+
+UH=$(cat tempFHome)
+UR=$(cat tempFRoot)
+mkfs.ext$UH /dev/sda4
+mkfs.ext$UR /dev/sda3
 mkfs.fat -F32 /dev/sda1
 mkswap /dev/sda2
 swapon /dev/sda2
@@ -27,6 +35,10 @@ mkdir /mnt/boot
 mount /dev/sda1 /mnt/boot
 mkdir /mnt/home
 mount /dev/sda4 /mnt/home
+rm tempRoot tempHome
+echo "------------------------------------------------------------"
+echo "RANKING MIRRORS PLEASE WAIT, THIS COULD TAKE A TIME"
+echo "------------------------------------------------------------"
 #------------------------------------------------------------
 #RANK MIRRORS
 #------------------------------------------------------------
@@ -36,12 +48,11 @@ rankmirrors -n 0 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
 
 #------------------------------------------------------------
 #installing base files for system
-pacman -Syy
 pacstrap /mnt base base-devel
 
 #generating fastab
 genfstab -U /mnt >> /mnt/etc/fstab
-
+cat tz.tmp > /mnt/tzfinal.tmp && rm tz.tmp
 #download and execute the second part of the script
 curl https://raw.githubusercontent.com/Kitsukai/kuroArch/master/shiroChroot.sh > /mnt/chroot.sh && arch-chroot /mnt bash chroot.sh && rm /mnt/chroot.sh
 
--- shiroChroot.sh
+++ shiroChroot.sh
@@ -2,7 +2,10 @@
 #second part of the kuroArch script
 passwd
 pacman -S --noconfirm dialog
-ln -sf /usr/share/zoneinfo/America/Bogota /etc/localtime
+
+TZuser=$(cat tzfinal.tmp)
+
+ln -sf /usr/share/zoneinfo/$TZuser /etc/localtime
 
 hwclock --systohc
 
@@ -15,15 +18,12 @@ function getZen() { pacman --noconfirm --needed -S linux-zen linux-zen-headers ;
 function zenOnly() { pacman -Rscn linux ;}
 function askForOnlyZen() { dialog --title "kuroArch Tweaks" --yesno "Do you want remove the generic linux kernel?"  8 60 && zenOnly ;}
 function zenSystem() { getZen && askForOnlyZen ;}
-#install and enable networkmanager
-pacman --noconfirm --needed -S networkmanager
-systemctl enable NetworkManager
-systemctl start NetworkManager
-
+function getNM() { pacman --noconfirm --needed -S networkmanager && systemctl enable NetworkManager && systemctl start NetworkManager ;}
 #ask for little tweaks
+#ask for install and enable networkmanager
+dialog --title "kuroArch Tweaks" --yesno "Do you want install Network Nanager?"  8 60 && getNM
 dialog --title "kuroArch Tweaks" --yesno "Do you want set keybord layout to es?"  8 60 && setKbEs
 dialog --title "kuroArch Tweaks" --yesno "Do you want install vim?"  8 60 && getVim
 dialog --title "kuroArch Tweaks" --yesno "Do you want install the linux-zen kernel?"  8 60 && zenSystem
-
 
 pacman --noconfirm --needed -S grub efibootmgr && grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB && grub-mkconfig -o /boot/grub/grub.cfg
